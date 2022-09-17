e2f7ff98145d6944ce8eec53029478eafeb5e03f
Leopen <790480953@qq.com>
2022-09-17 20:28:45 +0800

    feat: 从unpkg拉取packagejson并显示依赖页面的实现

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/axios',
+      name: 'Axios',
+      component: () => import('../views/Axios.vue'), // 懒加载 Axios 组件
+    },
   ],
 })
 
