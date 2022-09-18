cfa240f007fbc5c2c01911f998035ed7f938639a
Leopen <790480953@qq.com>
2022-09-18 21:30:28 +0800

    chore: vitest 支持 @ 指向 src

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 
@@ -8,5 +9,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
 })
